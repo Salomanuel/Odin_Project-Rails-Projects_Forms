3fb37f812768a4b0079bd652122b76ff40ade5b4
Salomanuel <salomanuel.aux+code@gmail.com>
2017-04-14 22:06:13 +0200

    user editing works!

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -17,8 +17,12 @@ class UsersController < ApplicationController
 
 	def edit
 		@user = User.find(params[:id])
-		if @user.save
-			redirect_to new_user_path
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to edit_user_path(@user)
 		else
 			render :edit
 		end
--- re-former/config/routes.rb
+++ re-former/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-	resources :users, :only => [:new,:create,:edit]
+	resources :users #, :only => [:new,:create,:edit]
 	# get '/users', to: 'users#index'
 end
