db5ee409f10232068ac32fe9b21eca6c1a400fc5
Salomanuel <salomanuel.aux+code@gmail.com>
2017-04-14 18:05:34 +0200

    add password to the users db

--- re-former/db/schema.rb
+++ re-former/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413221521) do
+ActiveRecord::Schema.define(version: 20170414130109) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "password"
   end
 
 end
