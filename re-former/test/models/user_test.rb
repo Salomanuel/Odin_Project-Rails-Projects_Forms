d60b52b900420ab419f85025374cbd811c6c9da7
Salomanuel <salomanuel.aux+code@gmail.com>
2017-04-14 12:00:59 +0200

    add basic test for user

--- re-former/test/models/user_test.rb
+++ re-former/test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	setup do
+		@user = User.new(username:"Johnny Cage", email: "example@user.com")
+	end
+
+  test "it should just work" do
+  	assert @user.valid?
+  end
 end
