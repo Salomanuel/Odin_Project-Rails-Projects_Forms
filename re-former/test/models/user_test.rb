8c3add9cb224f1db4b274ae6cd22cac40a6bae23
Salomanuel <salomanuel.aux+code@gmail.com>
2017-04-18 13:35:22 +0200

    add validating tests for attributes presence

--- re-former/test/models/user_test.rb
+++ re-former/test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = User.new(username: 	"Gianni",
+  									 email: 		"gianni@example.com",
+  									 password:  "moltogianni")
+  end
+
+  test "it should work" do
+  	assert @user.valid?
+  end
+
+  test "it should have an username" do
+  	@user.username = "    "
+  	assert_not @user.valid?
+  end
+
+  test "it should have an email" do
+  	@user.email = "    "
+  	assert_not @user.valid?
+  end
+
+  test "it should have a password" do
+  	@user.password = "     "
+  	assert_not @user.valid?
+  end
+
+  # test "it should have a"
 end
