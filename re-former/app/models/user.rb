7bf5cd9f14efc4304a0c93b5d9a1c51e5c006a46
Salomanuel <salomanue.aux+code@gmail.com>
2017-06-01 19:30:23 +0200

    in the user model add validations (tested)

--- re-former/app/models/user.rb
+++ re-former/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	validates :username, 	presence: true,
+												length: { maximum: 60 }
+	validates :email,			presence: true,
+												length: { maximum: 255 }
 end
--- re-former/test/models/user_test.rb
+++ re-former/test/models/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@user = User.new( username: "Manuel",
+											email: "manuel@manu.el")
+	end
+
+	test "user should work" do
+		assert @user.valid?
+	end
+
+	test "username should not be blank" do
+		@user.username = "    "
+		assert_not @user.valid?
+	end
+
+	test "username should not be too long" do
+		@user.username = "a" * 61
+		assert_not @user.valid?
+	end
+
+	test "email should not be blank" do
+		@user.email = "     "
+		assert_not @user.valid?
+	end
+
+	test "email should not be too long" do
+		@user.email = "a" * 255 + "@user.com"
+		assert_not @user.valid?
+	end
 end
