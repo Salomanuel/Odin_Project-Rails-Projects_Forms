78a10ddfa911f170a6508844074d72d158da098e
Salomanuel <salomanue.aux+code@gmail.com>
2017-06-03 16:24:26 +0200

    BASIC HTML FORM works

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new(username: params[:username], 
+												email: params[:email])
+		if @user.valid?
+			@user.save
+			render html: "SAVED"
+			# redirect_to new_user_path
+		else
+			render html: @user.errors.messages
+			# render :new
+		end
 	end
 end
+
+=begin
+":username"=>"user1", ":email"=>"user1@user.us"
+=end
