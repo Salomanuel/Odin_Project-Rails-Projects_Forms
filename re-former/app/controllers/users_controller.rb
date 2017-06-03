a6753a3caf6acafc31b72c998027f309cbb8c612
Salomanuel <salomanue.aux+code@gmail.com>
2017-06-03 20:06:01 +0200

    railsyer forms with form_for work as well!

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 	def new
+		@user = User.new
 	end
 
 	def create
-		@user = User.new(username: params[:username], 
-												email: params[:email])
-		# @user = User.new(user_params)
+		# @user = User.new(username: params[:username], 
+		# 										email: params[:email])
+		@user = User.new(user_params)
 		if @user.valid?
 			@user.save
 			render html: "SAVED"
