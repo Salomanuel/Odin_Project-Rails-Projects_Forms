cdd139d418377bf9f711e90a29dab4e95d1a61ea
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-06 15:52:44 +0200

    html form with #user_params works

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.valid?
+			@user.save
+			render inline:"<h1>ok</h1>"
+		else
+			render inline:"<h1>oh no</h1>"
+		end
 	end
+	private
+		def user_params
+			params.require(:user).permit(:username, :password, :email)
+		end
+
 end
--- re-former/app/models/user.rb
+++ re-former/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	validates :user, 			presence: true, length: { maximum: 20 }
-	validates :email			presence: true, length: { maximum: 40 }
-	validates :password   presence: true, length: { minimum: 6  }
+	validates :username, 	presence: true, length: { maximum: 20 }
+	validates :email,			presence: true, length: { maximum: 40 }
+	validates :password,  presence: true, length: { minimum: 6  }
 end
