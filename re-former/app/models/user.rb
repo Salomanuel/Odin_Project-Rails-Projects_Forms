cff27f6870f1fcc14ea53c9e47e700db9377dc6c
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-06 12:19:42 +0200

    add validation for User model

--- re-former/app/models/user.rb
+++ re-former/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :user, 			presence: true, length: { maximum: 20 }
+	validates :email			presence: true, length: { maximum: 40 }
+	validates :password   presence: true, length: { minimum: 6  }
 end
